826d2e65730e3d14a8b98d9ce9b2ac4d893d0752
determinist <victorbabajide8@gmail.com>
2025-08-02 22:14:30 +0100

    Updated directory for mock contract in import

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
-import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+import {MockV3Aggregator} from "../mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script {
     uint8 public constant DECIMALS = 8;
